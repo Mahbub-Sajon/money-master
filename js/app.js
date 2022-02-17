a94e5a566aa6f81b47303954a66477916ca19bbf
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-02-17 11:55:00 +0600

    function adding

---
+++ js/app.js
@@ -0,0 +1,14 @@
+document.getElementById('calculate-button').addEventListener('click',function(event){
+    event.preventDefault();
+    const totalIncome = document.getElementById('income-input');
+    const incomeAmount = totalIncome.value;
+    const foodExpense = document.getElementById('food-expense-input');
+    const foodExpenseAmount = foodExpense.value;
+    const rentExpense = document.getElementById('rent-expense-input');
+    const rentExpenseAmount = rentExpense.value;
+    const clothesExpense = document.getElementById('clothes-expense-input');
+    const clothesExpenseAmount = clothesExpense.value;
+    const totalExpensesAmount = parseFloat(foodExpenseAmount )  + parseFloat(rentExpenseAmount)  + parseFloat(clothesExpenseAmount);
+    console.log(totalExpensesAmount);
+   
+})

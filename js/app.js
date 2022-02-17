9f49fb3353649bc0ea32d90b47d9969d1776e0bf
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-02-17 16:39:49 +0600

    calculation updating

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-document.getElementById('calculate-button').addEventListener('click',function(event){
-    event.preventDefault();
+function getValues(inputValue){
     const totalIncome = document.getElementById('income-input');
     const incomeAmount = totalIncome.value;
     const foodExpense = document.getElementById('food-expense-input');
@@ -9,8 +8,33 @@ document.getElementById('calculate-button').addEventListener('click',function(ev
     const clothesExpense = document.getElementById('clothes-expense-input');
     const clothesExpenseAmount = clothesExpense.value;
     const totalExpensesAmount = parseFloat(foodExpenseAmount )  + parseFloat(rentExpenseAmount)  + parseFloat(clothesExpenseAmount);
+    const balance = parseFloat(incomeAmount)  - totalExpensesAmount;
+    const balanceAmount = document.getElementById('balance-amount');
+    const balanceAmountText = balanceAmount.innerText;
+    balanceAmount.innerText = parseFloat(balanceAmountText) + balance;
+
+
+    const expenseAmount = document.getElementById('expense-amount');
+   const expenseAmountText = expenseAmount.innerText;
+   expenseAmount.innerText = parseFloat(expenseAmountText)  + totalExpensesAmount;
+   totalIncome.value = '';
+    foodExpense.value = '';
+    rentExpense.value = '';
+    clothesExpense.value = '';
+    // return totalExpensesAmount;
+}
+
+
+document.getElementById('calculate-button').addEventListener('click',function(){
+    getValues()
+    
+    // const savingInput = document.getElementById('saving-input');
+    // const savingPercentage = savingInput.value;
+    // const savingAmount = incomeAmount * (savingPercentage / 100);
+    // console.log(savingAmount);
     
    
-})
+// })
+// // document.getElementById('save-button').addEventListener('click', function(){
 
-// incomeAmount * (percentage / 100)
+})
